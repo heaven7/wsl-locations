95dfb240edf40330746f8b341099b6ee7b71d324
Christian Grlica <christian@heavenseven.net>
2016-01-17 17:56:23 +0100

    search index

--- package.js
+++ package.js
@@ -1,37 +1,32 @@
 Package.describe({
   name: 'heaven7:wsl-locations',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Geo location package',
   git: 'https://github.com/heaven7/wsl-locations.git',
   documentation: 'README.md'
-});
+})
 
-both = ['client','server'];
+let both = ['client','server'],
+    packages = [
+        'heaven7:wsl-core@0.0.3_1',
+        'aldeed:geocoder@0.3.6'
+    ]
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.2');
-
-    var packages = [
-        'heaven7:wsl-core@0.0.3_1',
-        'heaven7:wsl-items@0.0.2',
-        'aldeed:geocoder@0.3.6'
-
-    //    'cordova:org.apache.cordova.geolocation@0.3.10'
-    ];
-
-    api.use(packages);
-    api.imply(packages);
+    api.versionsFrom('1.2')
+    api.use(packages)
+    api.imply(packages)
 
     api.addFiles([
         'lib/both/locations.js',
         'lib/both/schemas.js',
         'lib/both/helpers.js'
-    ], both);
+    ], both)
 
     api.addFiles([
         'lib/server/publish.js',
         'lib/server/methods.js'
-    ], ['server']);
+    ], ['server'])
 
     api.addFiles([
         'lib/client/helper.js',
@@ -39,17 +34,17 @@ Package.onUse(function(api) {
         'lib/client/templates.js',
         'lib/client/collection.js',
         'lib/client/hooks.js'
-    ], ['client']);
+    ], ['client'])
 
-    api.export(['Locations'], both);
+    api.export(['Locations'], both)
 
     Package.onTest(function(api) {
-        api.use('sanjo:jasmine@0.18.0');
-        api.use('velocity:console-reporter@0.1.3');
+        api.use('sanjo:jasmine@0.18.0')
+        api.use('velocity:console-reporter@0.1.3')
         api.use('velocity:html-reporter@0.8.2')
-        api.use('heaven7:wsl-locations');
+        api.use('heaven7:wsl-locations')
 
-        api.addFiles('tests/client/example-spec.js', ['client']);
-        api.addFiles('tests/server/example-spec.js', ['server']);
-    });
-});
+        api.addFiles('tests/client/example-spec.js', ['client'])
+        api.addFiles('tests/server/example-spec.js', ['server'])
+    })
+})
